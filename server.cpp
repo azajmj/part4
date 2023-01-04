86bd79f3fa587dfc558218a91cd53ba85f8a1b57
Karni <118285361+azajmj@users.noreply.github.com>
2023-01-04 21:05:15 +0200

    fixing server
    - deserializes data as needed
    - checks args
    fixed code

--- server.cpp
+++ server.cpp
@@ -9,100 +9,105 @@
 #include <vector>
 #include <cctype>
 #include "knn.h"
+#include <sstream>
 // #include <cstudio>
-
 #define MAX_MSG 100
 
 int main(int argc, char *argv[]) {
-    int port = atoi(argv[2]);
-    string file(argv[1]);
-    const int server_port = port;
-    int sock  = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("error creating socket");
-    }
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(server_port);
-    if (bind(sock, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
-        perror("error binding socket");
-    }
-    if (listen(sock, 5)) {
-        perror("error listening to socket");
-    }
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    while (true){
-        int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
-        if (client_sock < 0){
-            perror("error accepting client");
+    try {
+        // Check that there are exactly 2 arguments
+        if (argc != 3) {
+            throw invalid_argument("Expected 2 arguments: <dataset_file> <server_port>");
         }
-        char buffer[4096];
-        int expected_data_len = sizeof(buffer);
-        int read_bytes = recv(client_sock, buffer, expected_data_len,0);
-        if (read_bytes == 0) {
-            perror("error lost connection to cleint");
+        // Check that the first argument is a string
+        string file(argv[1]);
+        // Check that the second argument is an integer
+        int port = atoi(argv[2]);
+        if (port == 0) {
+            throw invalid_argument("Expected an integer value for <server_port>");
         }
-        else if (read_bytes < 0) {
-            perror("error reading from socket");
+        const int server_port = port;
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            perror("error creating socket");
         }
-        else {
-            // cout <<"the client sent: " << buffer << endl;
-            // int len = sizeof(buffer) / sizeof(buffer[0]);
-            // for(int i = 0; i < len; i++){
-            //     buffer[i] = toupper(buffer[i]);
-            // }
+        struct sockaddr_in sin;
+        memset(&sin, 0, sizeof(sin));
+        sin.sin_family = AF_INET;
+        sin.sin_addr.s_addr = INADDR_ANY;
+        sin.sin_port = htons(server_port);
+        if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+            perror("error binding socket");
+        }
+        if (listen(sock, 5)) {
+            perror("error listening to socket");
+        }
+        struct sockaddr_in client_sin;
+        unsigned int addr_len = sizeof(client_sin);
 
-            // get parameters from client buffer
-            int flag = 0;
-            int k;
-            string dist = "";
-            string pred = "";
-            vector<double> test;
-            try {
-                vector<string> tokens;
-                char* token = strtok(buffer, " ");
-                while (token != NULL) {
-                    string s = token;
-                    tokens.push_back(s);
-                    token = strtok(NULL, " ");
+            int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
+            if (client_sock < 0) {
+                perror("error accepting client");
+            }
+       while (true) {
+            char buffer[4096];
+            int expected_data_len = sizeof(buffer);
+            int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+            if (read_bytes == 0) {
+                perror("error lost connection to client");
+            } else if (read_bytes < 0) {
+                perror("error reading from socket");
+            } else {
+
+                cout << buffer <<endl;
+
+                // Deserialize the data
+                stringstream deserialized_data(buffer);
+                int vector_size;
+                vector<double> vec;
+                string pred = "";
+                deserialized_data >> vector_size;
+                for (int i = 0; i < vector_size; i++) {
+                    double number;
+                    deserialized_data >> number;
+                    vec.push_back(number);
                 }
-                k = stoi(tokens[tokens.size() - 1]);
-                dist = (tokens[tokens.size() - 2]);
-                for(int i = 0; i < tokens.size() - 2; i++) {
-                    double v = stof(tokens[i]);
-                    test.push_back(v);
-                }
-                    } catch (...) {
-                        perror("invalid input");
-                        flag = 1;
+                string dist;
+                int k;
+                deserialized_data >> dist;
+                deserialized_data >> k;
+                // Perform KNN classification:
+                try {
+                    KNNClassifier model(k, file, dist);
+                    if (model.getSampleSize() == vec.size()) {
+                        pred = model.predict(vec);
+                        if (!pred.empty()) {
+                            for (int i = 0; i < pred.size(); i++) {
+                                buffer[i] = pred[i];
+                            }
+                            buffer[pred.size()] = '\0';
+                        } else {
+                            strncpy(buffer, "no prediction", sizeof(buffer));
                         }
-            // knn
-            if (flag == 0){
-                KNNClassifier model(k, file, dist);
-                if (model.getSampleSize() == test.size()) {
-                    pred = model.predict(test);
-                    if (!pred.empty()) {
-                        for (int i = 0; i < pred.size(); i++){
-                            buffer[i] = pred[i];
-                        }
-                        buffer[pred.size()] = '\0';
                     } else {
-                        perror("no prediction");
-                    } 
-                } else {
-                    perror("test vector invalid size");
+                        strncpy(buffer, "invalid input", sizeof(buffer));
+                    }
+                } catch (...) {
+                    strncpy(buffer, "invalid input123", sizeof(buffer));
                 }
             }
+            //cout << buffer << endl;
+            int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+            if (sent_bytes < 0) {
+                perror("error sending to client");
+            }
         }
-        int sent_bytes = send(client_sock, buffer, read_bytes, 0);
-        if (sent_bytes < 0) {
-            perror("error sending to client");
-        }
+        close(sock);
     }
-    close(sock);
-    return 0;
+    catch (const exception &e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
 
+    return 0;
 }
