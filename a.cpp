5a5f217e7d1603f2750b113d02a20f55a6108f6a
Karni <118285361+azajmj@users.noreply.github.com>
2023-01-04 21:05:58 +0200

    put main in comments so code runs

--- a.cpp
+++ a.cpp
@@ -28,27 +28,27 @@ vector<double> getVector(string stringVector) {
     }
     return v;
 }
-
-int main(int argc, char *argv[]) {
-    cout << "Hello\n";
-    int k;
-    k = stoi( argv[1] );
-    string file = argv[2];
-    string distance = argv[3];
-    KNNClassifier model(k, file, distance);
-    while(true) {
-        string vec, pred;
-        vector<double> test;
-        getline(cin, vec);
-        if (vec == "finish") {
-            break;
-        }
-        test = getVector(vec);
-        if (model.getSampleSize() == test.size()) {
-            pred = model.predict(test);
-            cout << pred <<"\n";
-        }
-        else throw std::invalid_argument( "Invalid Size Input Vector" );
-    }
-    return 0;
-}
+//
+//int main(int argc, char *argv[]) {
+//    cout << "Hello\n";
+//    int k;
+//    k = stoi( argv[1] );
+//    string file = argv[2];
+//    string distance = argv[3];
+//    KNNClassifier model(k, file, distance);
+//    while(true) {
+//        string vec, pred;
+//        vector<double> test;
+//        getline(cin, vec);
+//        if (vec == "finish") {
+//            break;
+//        }
+//        test = getVector(vec);
+//        if (model.getSampleSize() == test.size()) {
+//            pred = model.predict(test);
+//            cout << pred <<"\n";
+//        }
+//        else throw std::invalid_argument( "Invalid Size Input Vector" );
+//    }
+//    return 0;
+//}
