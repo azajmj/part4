ac6237e54c92361bab9c5519831da3fc8a8c1d0b
Karni <118285361+azajmj@users.noreply.github.com>
2023-01-04 20:58:00 +0200

    updated client
    - checks to arguments and final fixes after debugging

--- client.cpp
+++ client.cpp
@@ -5,27 +5,34 @@
 #include <sstream>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <signal.h>
+
 
 using namespace std;
-// void segfault_handler(int sig) {
-//     cout << "invalid input seg" << endl;
-//     exit(1);
-// }
 
 int main(int argc, char *argv[]) {
-    int port = atoi(argv[2]);
-    //Create a socket:
-    // the socket function return an int which is the id(fd = file descriptor) of the socket.
-    // AF_INET - use ipv4 (1.2.3.4).
-    // SOCK_STREAM - TCP
-    while(true) {
+    try {
+            // Check that there are exactly 2 arguments
+            if (argc != 3) {
+                throw invalid_argument("Expected 2 arguments: <dataset_file> <server_port>");
+            }
+            // Check that the first argument is a string
+            string ip = (argv[1]);
+            // Check that the second argument is an integer
+            int port = atoi(argv[2]);
+            if (port == 0) {
+                throw invalid_argument("Expected an integer value for <server_port>");
+            }
+
+        //Create a socket:
+        // the socket function return an int which is the id(fd = file descriptor) of the socket.
+        // AF_INET - means use ipv4 (1.2.3.4).
+        // SOCK_STREAM - means TCP
+
         int sock = socket(AF_INET, SOCK_STREAM, 0);
         if (sock < 0) {
             cout << "Error creating socket" << endl;
             return -1;
         }
-    
 
         //Connect to the server:
         // define all the details:
@@ -36,101 +43,120 @@ int main(int argc, char *argv[]) {
         // family = ip type.
         addr.sin_family = AF_INET;
         // set the ip address of the server:
-        // todo - we will use 127.0.0.1 ip for now, in order to allow a connection from this computer using 2 terminals
-        // todo - we will need to change it to the user input ip.
         // we will cast the ip address to inet_addr.
-        // "127.0.0.1"
         addr.sin_addr.s_addr = inet_addr(argv[1]);
         // cast the port to htons.
-        // todo - we will need to change it to the user input port.
         addr.sin_port = htons(port);
 
         // explicit casting from sockaddr_in to sockaddr*
-        //        int x = 5;
-        //        float y = (float)x;
-        //        float* z = (float*)&x;
-        //
-        //        sockaddr_in addr;
-        //        sockaddr y = (sockaddr)addr;
-        if (connect(sock,(sockaddr*)&addr, sizeof (addr)) < 0 ){
+        // like saying:  int x = 5;
+        //              float y = (float)x;
+        //              float* z = (float*)&x;
+        // here its equivalent:
+        //              sockaddr_in addr;
+        //              sockaddr y = (sockaddr)addr;
+        if (connect(sock, (sockaddr *) &addr, sizeof(addr)) < 0) {
             cout << "Error connecting to the server" << endl;
             return -1;
         }
 
-        //todo - this is just for now, in order to check the connection.
-        // need to change to the input: vector,metric and k.
-    
-        int flag = 0;
-        string message = "";
-        while(flag == 0) {
-            getline(cin, message);
-            if (message == "-1") {
-                flag = 2;
-                break;
+
+        while (1) {
+            vector<double> numbers;
+            string dist;
+            int k;
+            //cout << "Enter your data" << endl;
+            string input;
+            getline(cin, input);
+            // "input looks something like =  "1 2 3 4 AUC 1"
+            istringstream stream_str(input); //keeps string in input stringstream
+            if (input == "-1") {
+                close(sock);
+                return 0;
             }
-            try {
-                // signal(SIGSEGV, segfault_handler);
-                cout << "in try catch" << endl;
-                vector<string> tokens;
-                vector<double> test;
-                char copy_message[sizeof(message)];
-                copy(message.begin(), message.end(), copy_message);
-                copy_message[message.size()] = '\0';
-                char* token = strtok(copy_message, " ");
-                while (token != NULL) {
-                    string s = token;
-                    tokens.push_back(s);
-                    token = strtok(NULL, " ");
+            int flag = 0;
+            while (stream_str.good()) {
+                string token;
+                getline(stream_str, token, ' ');
+                double val = 0;
+                try
+                    //trying to convert vector to double
+                {
+                    val = stod(token.c_str());
+                    numbers.push_back(val);
                 }
-                if (tokens.size() >= 3) {
-                    int k = stoi(tokens[tokens.size() - 1]);
-                    string dist = (tokens[tokens.size() - 2]);
-                    for(int i = 0; i < tokens.size() - 2; i++) {
-                        double v = stof(tokens[i]);
-                        test.push_back(v);
-                    }
-                flag = 1;
-                } else {
-                    cout << "invalid input" << endl;
-                }
-            } catch (...) {
+                catch (exception &e) {
+                    //making sure token is one of the distances
+                    if (token == "AUC" || token == "MIN" || token == "MAN" || token == "CAN" || token == "CHB") {
+                        dist = token;
+                    } else {
                         cout << "invalid input" << endl;
-                        }
-        }
-        // string message = "2 3 4 5 MAN 3";
-        // c_str() convert the string from string to const char*, as needed in send function.
-        if (flag == 1) {
-            cout << "in send message" << endl;
-            if (send(sock,message.c_str(), message.size(), 0) < 0){
+                        flag = 1;
+                        break;
+                    }
+
+                    // after we took the dist, the stream string contain only the k value.
+                    // so now we can just take the last value and save him in k variable.
+                    getline(stream_str, token, ' ');
+                    try {
+                        k = stoi(token);
+                        break;
+                    }
+                    catch (exception &e) {
+                        cout << "invalid input" << endl;
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+
+            //checking vector input
+            if (numbers.size() == 0) {
+                cout << "invalid input" << endl;
+                continue;
+            }
+            if (flag == 1) {
+                continue; //invalid input, new iteration
+            }
+            // we want to do Serialization to our input in order to send it to the server:
+            // we will use string stream:
+            // 4 1 2 3 4 AUC 2
+            stringstream data;
+            data << numbers.size() << " ";
+            for (int i = 0; i < numbers.size(); i++) {
+                data << numbers[i] << " ";
+            }
+            data << dist << " ";
+            data << k;
+            string message = data.str();
+
+            // c_str() convert the string from string to const char*, as needed in send function.
+            if (send(sock, message.c_str(), message.size(), 0) < 0) {
                 cout << "Error sending message " << endl;
                 return -1;
             }
-            
-
             //Read the Response from the server:
             // create a buffer (char array in size of 1024 bits) to save the server response.
             char buffer[1024];
             // we will use the recv function:
             // the data we received will save in the buffer, and the return value is the size.
-            int bytesReceived = recv(sock,buffer,1024,0);
-            if ( bytesReceived < 0) {
+            int bytesReceived = recv(sock, buffer, 1024, 0);
+            if (bytesReceived < 0) {
                 cout << "Error receiving message from server " << endl;
                 return -1;
             }
-            cout << "read servers response message" << endl;
-
-            // Print the response from the server:
-            cout << "Response from server: " << string(buffer,bytesReceived) << endl;
-            //Close the socket:
-            close(sock);
-        }
-        if (flag == 2) {
-            close(sock);
-            break;
+            //cout << "Response from server: ";
+            int index = 0;
+            while(buffer[index] != '\0')
+            {
+                cout<< buffer[index];
+                index++;
+            }
         }
     }
-    // close(sock);
+    catch (const exception &e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
     return 0;
 }
-
-// todo list: 1. user input. 2. input validation. 3. send the user input instead of message. 4. infinite loop
